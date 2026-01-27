433efe0a7a2ab6d8f487a69484605b621c400fc9
Your Name <yassine19khadir@gmail.com>
2026-01-27 12:47:35 +0100

    Add implementations for Daily Challenge 1 and Daily Challenge 2

--- geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
+++ geeks_institute_bootcamp/week_1/day_1/daily_challenge/challenges.js
@@ -1,5 +1,20 @@
 // ====== Daily Challenge 1
+let sentence = "The movie is not that bad, I like it";
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
 
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+  let beforeNot = sentence.slice(0, wordNot);
+  let afterBad = sentence.slice(wordBad + 3);  
+  let newSentence = beforeNot + "good" + afterBad;
+  console.log(newSentence);  
+} else {
+  console.log(sentence);
+}
 // ====== Daily Challenge 2
+let stars = "";
+for (let i = 1; i <= 6; i++) {
+  stars = stars + "* ";
+  console.log(stars);
+}
 
-// ====== Daily Challenge 3
