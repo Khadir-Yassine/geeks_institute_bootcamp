085738d386c4ed695f3669299c18053517f47a20
Your Name <yassine19khadir@gmail.com>
2026-02-04 10:05:24 +0100

    Add initial implementation of groceries management with display and clone functions

--- geeks_institute_bootcamp/week_1/day_4/daily_challenge/challenge_1.js
+++ geeks_institute_bootcamp/week_1/day_4/daily_challenge/challenge_1.js
@@ -1,1 +1,39 @@
+let client = "John";
+
+const groceries = {
+  fruits: ["pear", "apple", "banana"],
+  vegetables: ["tomatoes", "cucumber", "salad"],
+  totalPrice: "20$",
+  other: {
+    paid: true,
+    meansOfPayment: ["cash", "creditCard"]
+  }
+};
+
+const displayGroceries = () => {
+  groceries.fruits.forEach((fruit) => {
+    console.log(fruit);
+  });
+};
+const cloneGroceries = () => {
+  let user = client;
+
+  client = "Betty";
+
+  console.log("client:", client); 
+  console.log("user:", user);     
+
+  let shopping = groceries;
+
+  shopping.totalPrice = "35$";
+
+  shopping.other.paid = false;
+
+  console.log("groceries:", groceries);
+  console.log("shopping:", shopping);
+};
+
+/* Invoke functions */
+displayGroceries();
+cloneGroceries();
 
