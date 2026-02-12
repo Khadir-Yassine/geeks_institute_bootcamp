e24576d76e238c674133562d60d7f6838d4c83f6
Your Name <yassine19khadir@gmail.com>
2026-02-12 11:48:39 +0100

    .

---
+++ geeks_institute_bootcamp/week_2/day_2_Advenced_Array_Methods/chanllenge/Go_Widcat.js
@@ -0,0 +1,25 @@
+const gameInfo = [
+  { username: "john", team: "red", score: 5, items: ["ball","book","pen"] },
+  { username: "becky", team: "blue", score: 10, items: ["tape","backpack","pen"] },
+  { username: "susy", team: "red", score: 55, items: ["ball","eraser","pen"] },
+  { username: "tyson", team: "green", score: 1, items: ["book","pen"] },
+];
+
+const usernames = [];
+const winners = [];
+let totalScore = 0;
+
+gameInfo.forEach(function(player) {
+  
+  usernames.push(player.username + "!");
+  
+  if (player.score > 5) {
+    winners.push(player.username);
+  }
+  
+  totalScore += player.score;
+});
+
+console.log("Usernames:", usernames);
+console.log("Winners:", winners);
+console.log("Total score:", totalScore);
